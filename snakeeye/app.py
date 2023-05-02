4e820614f17ac41ccb75986b0d818e42de815c54
Jevria <jevriya.guliyeva@gmail.com>
2023-05-02 12:49:57 +0100

    Add first test cases

--- snakeeye/app.py
+++ snakeeye/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from blueprints.page import page
 
+
 def create_app():
     """
     Create a Flask application using the app factory pattern.
@@ -9,7 +10,7 @@ def create_app():
     """
     # By writing app = Flask(__name__), we name the flask instance as app.
     app = Flask(__name__, instance_relative_config=True)
-    #loading cofig module
+    # Loading config module
     app.config.from_object('config.settings')
     # silent=True telling not to crush if setting.py does not exist
     app.config.from_pyfile('settings.py', silent=True)
